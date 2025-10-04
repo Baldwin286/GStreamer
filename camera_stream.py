66b0f105796a9e5d2b068d5f007926b4792b674b
PhamThanhBien <thanhbienpham2003@gmail.com>
2025-10-04 10:11:39 +0700

    update stream12

--- camera_stream.py
+++ camera_stream.py
@@ -1,5 +1,5 @@
 import cv2
-import face_recognition
+import time
 import numpy as np
 
 face_detect = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
@@ -9,7 +9,7 @@ if face_detect.empty():
 cap = cv2.VideoCapture(0)  
 gst_str = (
     "appsrc ! videoconvert ! "
-    "x264enc tune=zerolatency bitrate=4000 speed-preset=superfast ! "
+    "x264enc tune=zerolatency bitrate=1000 speed-preset=superfast ! "
     "rtph264pay config-interval=1 pt=96 ! "
     "udpsink host=192.168.1.100 port=5000"
 )
@@ -30,8 +30,7 @@ print(f"Camera: {width}x{height} @ {fps}fps")
 out = cv2.VideoWriter(
     gst_str,
     cv2.CAP_GSTREAMER,
-    0,          
-    # 30,        
+    0,                
     # (640, 480), 
     # (320, 240),
     fps,
@@ -43,13 +42,18 @@ if not cap.isOpened() or not out.isOpened():
     print("Camera/GStreamer pipeline Error!")
     exit()
 
+prev_time = time.time()
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # resize_frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    resize_frame = cv2.resize(frame, frame_size)
+    curr_time = time.time()
+    fps_calc = 1 / (curr_time - prev_time)
+    prev_time = curr_time
+    print(f"FPS: {fps_calc:.2f}")
+
+    resize_frame = cv2.resize(frame, frame_size, interpolation=cv2.INTER_AREA)
     gray = cv2.cvtColor(resize_frame, cv2.COLOR_BGR2GRAY)
     face_detection = face_detect.detectMultiScale(gray,1.3,5)
     for (x,y,w,h) in face_detection:
