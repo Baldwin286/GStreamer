aa9bcecad8068dc868d280c56befc77f70241453
PhamThanhBien <thanhbienpham2003@gmail.com>
2025-10-05 21:19:04 +0700

    update v1

--- receive_stream.py
+++ receive_stream.py
@@ -1,25 +1,12 @@
-import cv2
+import subprocess
 
-pipeline = (
-    'udpsrc port=5000 caps="application/x-rtp,encoding-name=H264,payload=96" ! '
-    'rtph264depay ! avdec_h264 ! videoconvert ! appsink'
-)
+gst_command = [
+    r"C:\Program Files\gstreamer\1.0\msvc_x86_64\bin\gst-launch-1.0.exe",
+    "-v",
+    "udpsrc", "port=5000", "caps=application/x-rtp,encoding-name=H264,payload=96",
+    "!", "rtph264depay",
+    "!", "avdec_h264",
+    "!", "autovideosink"
+]
 
-cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-
-if not cap.isOpened():
-    print("Không mở được luồng video")
-    exit()
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Không nhận được frame")
-        break
-
-    cv2.imshow("Video từ Drone", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+subprocess.run(gst_command)
