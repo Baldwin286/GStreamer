a48500a5a209d25a27d8ca3822bfd05197746a91
PhamThanhBien <thanhbienpham2003@gmail.com>
2025-10-08 21:12:56 +0700

    Update RPI

--- RPI_V3.py
+++ RPI_V3.py
@@ -9,7 +9,7 @@ def stream_and_detect():
     if face_detect.empty():
         raise IOError("Unable to load the face cascade classifier xml file")
 
-    # Pipeline GStreamer cho Raspberry Pi Camera Module 3
+    
     gst_cam = (
         f"libcamerasrc ! video/x-raw,width={width},height={height},framerate=30/1 ! "
         "videoconvert ! appsink"
@@ -17,16 +17,16 @@ def stream_and_detect():
 
     cap = cv2.VideoCapture(gst_cam, cv2.CAP_GSTREAMER)
 
-    # Pipeline gửi stream qua UDP
+   
     gst_str = (
         "appsrc ! videoconvert ! "
         "x264enc tune=zerolatency bitrate=1000 speed-preset=superfast ! "
         "rtph264pay config-interval=1 pt=96 ! "
-        "udpsink host=192.168.1.100 port=5000"
+        "udpsink host=192.168.1.101 port=5000"
     )
 
     frame_size = (width, height)
-    fps = 30  # gán cứng vì nhiều khi libcamera không trả về FPS chính xác
+    fps = 30  
 
     print(f"Camera: {width}x{height} @ {fps}fps")
 
--- receive_stream.py
+++ receive_stream.py
@@ -3,7 +3,8 @@ import subprocess
 gst_command = [
     r"C:\Program Files\gstreamer\1.0\msvc_x86_64\bin\gst-launch-1.0.exe",
     "-v",
-    "udpsrc", "port=5000", "caps=application/x-rtp,encoding-name=H264,payload=96",
+    "udpsrc", "port=5000",
+    "caps=application/x-rtp,encoding-name=H264,payload=96",
     "!", "rtph264depay",
     "!", "avdec_h264",
     "!", "autovideosink"
